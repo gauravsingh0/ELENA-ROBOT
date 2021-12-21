0080b31a6ac9d86d4ece3fd5c0611740bc5eab21
gauravsingh0 <84321170+gauravsingh0@users.noreply.github.com>
2021-12-21 14:44:49 +0530

    Update __main__.py

--- Yuriko/__main__.py
+++ Yuriko/__main__.py
@@ -79,7 +79,7 @@ def get_readable_time(seconds: int) -> str:
 
     return ping_time
 
-yurikorobot_IMG = "https://telegra.ph/file/8b6f8f2bb4ff3912634c7.jpg"
+yurikorobot_IMG = "https://telegra.ph/file/cac9c99b007221927659f.jpg"
 
 PM_START_TEXT = """
 *👋 Hello {} !*

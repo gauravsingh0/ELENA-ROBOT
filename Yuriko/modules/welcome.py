c106016264653212e7038beb45b37fb7baecadde
gauravsingh0 <84321170+gauravsingh0@users.noreply.github.com>
2021-12-21 19:17:09 +0530

    Update welcome.py

--- Yuriko/modules/welcome.py
+++ Yuriko/modules/welcome.py
@@ -1267,7 +1267,7 @@ __help__ = """
 
 ✗ /welcomehelp - `view more formatting information for custom welcome/goodbye messages.`
 
-*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ DᴇCᴏᴅᴇ!*
+*✗ Pᴏᴡᴇʀᴇᴅ 💕 Bʏ: Tᴇᴀᴍ GᴀᴜʀBᴏᴏ!*
 """
 
 NEW_MEM_HANDLER = MessageHandler(

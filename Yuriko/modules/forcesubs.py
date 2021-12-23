b8ac435e477b93e665c07835105d92e8deccb421
gauravsingh0 <84321170+gauravsingh0@users.noreply.github.com>
2021-12-24 02:02:43 +0530

    Update forcesubs.py

--- Yuriko/modules/forcesubs.py
+++ Yuriko/modules/forcesubs.py
@@ -109,7 +109,7 @@ def _check_member(client, message):
                     )
                 except ChatAdminRequired:
                     sent_message.edit(
-                        "😕 **Yuriko is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
+                        "😕 **Elena is not admin here..**\n__Give me ban permissions and retry.. \n#Ending FSub...__"
                     )
 
             except ChatAdminRequired:
